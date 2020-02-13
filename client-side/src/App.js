5b69dba11c1db71853da50bdba8fc5bed2c3ddfc
unknown <ibrahimelwazer123@gmail.com>
2020-02-13 12:32:46 +0200

    CSS added to Login

--- client-side/src/App.js
+++ client-side/src/App.js
@@ -2,8 +2,6 @@ import React, { useState } from 'react';
 import Login from './auth/Login';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import StudentsHome from './components/StudentsHome';
-import PrivateRoute from './PrivateRoute';
-import { AuthContext }  from './auth/auth';
 import TeachersHome from './components/TeachersHome';
 import axios from 'axios';
 import './App.css';
--- client-side/src/auth/Login.js
+++ client-side/src/auth/Login.js
@@ -47,26 +47,33 @@ handleChange(e) {
 
     render() {
         return (
-            <div className="container">
-                <div className="row">
-                    <div className="col-md-6 mt-5 mx-auto">
-                    <h1 className="text-align mb-5">EXAM PORTAL</h1>
-                        <form onSubmit={this.handleSubmit}>
-                            <h1 className="h3 mb-4 font-weight-normal">Login With Your Account</h1>
-                            <div className="form-group">
-                                <label htmlFor="email">Email Address</label>
-                                <input type="email" className="form-control" required name="email" placeholder="Your Associated Email Address" value={this.state.email} onChange={this.handleChange}></input>
-                            </div>
-                            <div className="form-group">
-                                <label htmlFor="password">Password</label>
-                                <input type="password" className="form-control" required name="password" placeholder="Your Account's Password" value={this.state.password} onChange={this.handleChange}></input>
-                            </div>
-                            <button type="submit" className="btn btn-primary btn-block">Login</button>
-                        </form>
-                    </div>
+        <div class="container">
+            <div class="row">
+              <div class="col-sm-9 col-md-7 col-lg-5 mx-auto">
+              <h1 class="card-title text-center mt-4">EXAM PORTAL</h1>
+                <div class="card card-signin my-5">
+                  <div class="card-body">
+                    <form class="form-signin" onSubmit={this.handleSubmit}>
+                        <h1 className="h3 mb-4 font-weight-normal">Login With Your Account</h1>
+                        <div class="form-label-group">
+                            <label htmlFor="email">Email Address</label>
+                            <input type="email" className="form-control" required name="email" placeholder="Your Associated Email Address" value={this.state.email} onChange={this.handleChange}></input>
+                        </div>
+                        <div class="form-label-group mt-3">
+                            <label htmlFor="password">Password</label>
+                            <input type="password" className="form-control" required name="password" placeholder="Your Account's Password" value={this.state.password} onChange={this.handleChange}></input>
+                      </div>
+                            <button class="btn btn-lg btn-primary btn-block text-uppercase" type="submit">Login</button>
+                    </form>
+                  </div>
                 </div>
-            </div>  
+              </div>
+            </div>
+          </div>
         );
     }
 }
 export default Login;
+
+
+
